ed5f8b27c9f7c6b0b51495d1d203b060ace21777
Abhishek Kulkarni <53334823+abhishekkulkarni24@users.noreply.github.com>
2019-11-13 12:54:37 +0530

    Create welcome.java

---
+++ welcome.java
@@ -0,0 +1,20 @@
+public class welcome
+ 5  {
+ 6      // main method begins execution of Java application
+ 7      public static void main( String[] args )
+ 8      {
+ 9         System.out.println( "Welcome to Java Programming!" );
+10      } // end method main
+11  } // end class welcome
+
+/*
+Welcome to the repository. we start first basic program of java to print welcome message
+
+In java file name is same as the class name
+
+here we use public static void main( String[] args ) because program execution starts from main method
+
+function in java called as method
+
+for print in java System.out.println( "Welcome to Java Programming!" ); is used
+*/
