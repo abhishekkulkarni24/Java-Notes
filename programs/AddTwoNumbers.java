e1414a83e656e65125bafb0bad2f83cf1631fc66
Abhishek Kulkarni <53334823+abhishekkulkarni24@users.noreply.github.com>
2019-11-15 20:41:39 +0530

    Create AddTwoNumbers.java

---
+++ programs/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+public class AddTwoNumbers // create class
+{ 
+
+   public static void main(String[] args)  //create main method
+   {
+        
+      int num1 = 5, num2 = 15, sum; // declare and initialize variables
+      sum = num1 + num2; //store sum of two variables to sum variable
+
+      System.out.println("Sum of these numbers: "+sum); //print sum of two variables
+   }
+}
